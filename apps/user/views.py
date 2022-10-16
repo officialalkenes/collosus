b60dca1b68c84bf52cce6f1ca1fd7749a22763ed
officialalkenes <61650835+officialalkenes@users.noreply.github.com>
2022-10-16 13:47:06 +0100

    chore: update user views

--- apps/user/views.py
+++ apps/user/views.py
@@ -106,3 +106,85 @@ def my_login_page(request):
 def logout_view(request):
     logout(request)
     return redirect("user:login")
+
+
+# @login_required
+# def user_update(request, pk):
+#     context ={}
+#     obj = get_object_or_404(Profile, id=pk)
+#     form = UserEditForm()
+#     if request.method == 'POST':
+#         form = UserEditForm(request.POST, request.FILES, instance = obj)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, "User Profile Information Has been Updated Successfully")
+#             return redirect('investment:user-profiles')
+
+#     context["form"] = form
+#     return render(request, "user/create-profiles.html", context)
+
+
+@login_required
+def delete_user(request):
+    if request.method == "POST":
+        user = User.objects.get(username=request.user)
+        user.is_active = False
+        user.save()
+        delete_message = "Account will be rendered inactive for 3days before deleting. if you wish to recover account, Please contact the admin or use the reactivate account_link"
+        messages.success(request, delete_message)
+        return redirect("accounts:login")
+
+    return render(request, "user/delete.html")
+
+
+def activate_account_page(request, uidb64, token):
+    User = get_user_model()
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.email_verified = True
+        user.save()
+        login_attempt, created = LoginAttempt.objects.get_or_create(user=user)
+        if login_attempt.login_attempts >= settings.MAX_LOGIN_ATTEMPTS:
+            login_attempt.login_attempts = 0
+            login_attempt.save()
+            messages.success(request, "Account restored, you can now proceed to login")
+        else:
+            messages.success(
+                request,
+                "Thank you for confirming your email. You can now proceed to Required Registration.",
+            )
+        return redirect("accounts:login")
+    else:
+        messages.error(
+            request,
+            "Thank you for confirming your email. You can now proceed to Required Registration.",
+        )
+        return redirect("accounts:login")
+
+
+def change_password(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, "Your password was successfully updated!")
+            return redirect("bank:dashboard")
+        else:
+            messages.error(request, "Please correct the error below.")
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, "user/change_password.html", {"form": form})
+
+
+def handler404(request, exception):
+    return render(request, "user/404.html")
+
+
+def handle_server_error(request):
+    return render(request, "user/500.html")
