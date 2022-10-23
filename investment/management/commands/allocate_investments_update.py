6323e3c82c9b8a9256156a92adbd08324267759d
officialalkenes <61650835+officialalkenes@users.noreply.github.com>
2022-10-23 17:39:11 +0100

    chore: sort managements call commands

--- investment/management/commands/allocate_investments_update.py
+++ investment/management/commands/allocate_investments_update.py
@@ -27,26 +27,26 @@ class Command(BaseCommand):
                         percentage = investment.percentage * investment.amount
                         investment.profit += percentage
                         investment.save()
-                        mail_subject = "Investment Update"
-                        amount = investment.amount
-                        profit = investment.profit
-                        email = user.email
-                        total = amount + profit
-                        end_date = investment.end_date
+                        # mail_subject = "Investment Update"
+                        # amount = investment.amount
+                        # profit = investment.profit
+                        # # email = user.email
+                        # total = amount + profit
+                        # end_date = investment.end_date
                         self.stdout.write(
                             f"User {user.username}'s Investment Updated successfully!"
                         )
 
-                        send_investment_update(
-                            user,
-                            mail_subject,
-                            amount,
-                            email,
-                            profit,
-                            total,
-                            end_date,
-                            "users/email_investment_update.html",
-                        )
+                        # send_investment_update(
+                        #     user,
+                        #     mail_subject,
+                        #     amount,
+                        #     email,
+                        #     profit,
+                        #     total,
+                        #     end_date,
+                        #     "users/email_investment_update.html",
+                        # )
                     else:
                         self.stdout.write("No Investment Updated Available")
 
